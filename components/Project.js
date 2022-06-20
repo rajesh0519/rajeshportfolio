1e6d3cb3c1009c52b0737c5c0ff117a3ea40c98f
Rajesh Rawat <rawatrajesh0519@gmail.com>
2022-06-20 12:31:30 +0530

    1.3

--- components/Project.js
+++ components/Project.js
@@ -73,7 +73,7 @@ export default function Project(){
                                         </div>
 
                                         <div>
-                                            <a href={`https://${curElm.name}`} target="_blank" refs="noopener noreferre">{curElm.name}</a>
+                                            <a href={`https://${curElm.name}`} target="_blank" refs="noreferrer">{curElm.name}</a>
                                         </div>
 
                                         <div>
